0086cbf1ba5d6fbf78aaa47b58b8fdd9f30bd276
Artiom <artiom@flexlabs.org>
2018-12-06 22:53:59 +0000

    + Adding App Insights

--- src/CFProxy.API/Program.cs
+++ src/CFProxy.API/Program.cs
@@ -19,6 +19,7 @@ namespace CFProxy.API
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .UseApplicationInsights()
                 .UseStartup<Startup>();
     }
 }
